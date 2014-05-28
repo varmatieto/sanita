350de4ee7a59c087ce6cf091cedfcad2752cb918
varmatieto <ninobixio22@gmail.com>
2014-05-28 21:37:38 +0200

    nuove elaborazioni

--- elab_sanita.R
+++ elab_sanita.R
@@ -15,21 +15,25 @@ str(BS)                                 # 59 branche
 names(BS) <- c("id" ,"cbranca","bdescr")
 names(BS)                   # "id"      "cbranca" "bdescr" 
 head(BS)
-
+BS<-BS[order(BS$cbranca),]
 ###################
 CP<- read.table ("Epres.csv" , header=T, 
                  sep=";", dec=",",   stringsAsFactors=F)
-str(CP)                                 # 59 branche
+str(CP)                                 # 2337 obs.
 CP$cpres<-as.factor(CP$cpres)
 CP$cdiscip<-as.factor(CP$cdiscip)
 
 names(CP)                   #  "cdiscip" "cpres"   "descr"  
 head(CP)
-str(CP)   
+str(CP)
+barplot(table(CP$cpres))
 
+CP$descr[CP$cdiscip==1]
 
+length(unique(CP$cpres))
 ##########################
-pres<- read.table ("Dettaglio_Cod_Prest.csv" , header=T, sep=";")
+pres<- read.table ("Dettaglio_Cod_Prest.csv" , header=T, sep=";",
+                   stringsAsFactors=F)
 
 
 names(pres) <- c("id" ,"azienda", "tipo", "cbranca","cpres", "qta" )
@@ -37,18 +41,40 @@ pres$azienda<-as.factor(pres$azienda)
 pres$cpres<-as.factor(pres$cpres)
 pres$cbranca<-as.factor(pres$cbranca)
 
+
+
+
 str(pres)                              # 295643 obs. of  6 variables
 head(pres)                              # 295643 obs. of  6 variables
 
 
 levels(pres$cbranca)
-BS$bdescr[!BS$cbranca%in%levels(pres$cbranca)]
+branche_mancanti<-BS[!BS$cbranca%in%levels(pres$cbranca),2:3]
+
+dim(branche_mancanti)
 
 
-
+##################################################################
 ggplot(data=pres, aes(azienda, fill = tipo)) + 
   geom_bar(show_guide = F) + 
   ggtitle("frequency of different products") 
 
 
+##################################################
+presuro<- pres[pres$cbranca==43,]
+presuro$azienda<-as.factor(presuro$azienda)
+presuro$cpres<-as.factor(presuro$cpres)
+presuro$cbranca<-as.factor(presuro$cbranca)
+
+str(presuro)
+
+cpresuro<-unique(presuro$cpres)
+table(presuro$cpres)
+
+CP[CP$cpres%in%levels(cpresuro),])
+
+CP$cpres[1:10]
+ggplot(data=presuro, aes(azienda, fill = tipo)) + 
+    geom_bar(show_guide = F) + 
+    ggtitle("urologia in piemonte") 
 
